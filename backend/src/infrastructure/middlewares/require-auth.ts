1e1ef4f8ed943c4b34c4a2b2d1962d2e626208a3
George Cuevas <george@Georges-Laptop.local>
2023-05-01 20:46:07 -0400

    fix: use jwt algorith in env var

--- backend/src/infrastructure/middlewares/require-auth.ts
+++ backend/src/infrastructure/middlewares/require-auth.ts
@@ -5,5 +5,5 @@ const secret = process.env.JWT_KEY
 const algorithm = process.env.JWT_ALGO as Algorithm
 
 export function requireAuth() {
-    return expressjwt({ secret, algorithms: ["HS256"] })
+    return expressjwt({ secret, algorithms: [algorithm] })
 }

ca615953cd94ccabab5019c98dc9fafa25308aa5
George Cuevas <george@Georges-Laptop.local>
2023-05-04 14:06:41 -0400

    feat: add operation service test file

--- backend/src/domain/services/RandomStrService.ts
+++ backend/src/domain/services/RandomStrService.ts
@@ -8,6 +8,8 @@ export interface IRandomStrService {
 export class RandomStrService implements IRandomStrService {
 
     getValue(): string {
+
+        // NOTE: third party service will call here
         return (Math.random() + 1).toString(36).substring(7)
     }
 }
